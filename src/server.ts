f03993c28152f499b1040c84376fdb578e63d759
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-12-12 15:38:52 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import express, { json } from "express";
+import express, { json } from 'express';
 
 const app = express();
 
 app.use(json());
 
-app.listen(3333, () => console.log("🚀 App is running at port 3333!"));
-
-console.log("teste");
+app.listen(3333, () => console.log('🚀 App is running at port 3333!'));
